d0aa9d564e48181e0d79813256817ab7c157d5b2
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-03-26 18:39:53 -0300

    Movie list & Movie card components and Movies endpoint

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const nextConfig = { 
+  reactStrictMode: true, 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'http',
+        hostname: 'uhdtv.io',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'mango.blender.org',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'download.blender.org',
+        port: '',
+        pathname: '/**'
+      },
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
 import Navbar from '@/components/Navbar';
+import useMovieList from '@/hooks/useMovieList';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
 
@@ -19,11 +21,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Home = () => {
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className='pb-40'>
+        <MovieList title='Trending Now' data={movies} />
+      </div>
     </>
   );
 };
