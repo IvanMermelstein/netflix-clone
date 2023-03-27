7b840f820a498a75d3d3f987e2a471ec06931a9b
IvanMermelstein <ivan.mermelstein@gmail.com>
2023-03-27 17:10:40 -0300

    Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({
   Component, pageProps
 }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Netflix Clone</title>
+      <link rel="shortcut icon" href="/favicon.ico"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
